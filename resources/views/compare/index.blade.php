9489ad9c8124a07f209a2e76f912838143dd459f
Azim Sofi <azimmiskandar@gmail.com>
2024-10-28 11:00:04 +0900

    Typo

--- resources/views/compare/index.blade.php
+++ resources/views/compare/index.blade.php
@@ -7,10 +7,10 @@
 
     <h2>Compare Japanese Text</h2>
     <ul>
-        @foreach ($input_texts as $input_text)
+        @foreach ($compare_input_texts as $compare_input_text)
             <li>
-                <a href="{{ route('compare.show', $input_text->id) }}">
-                    {{ Str::limit($input_text->content, 50) }} <!-- Display first 50 characters -->
+                <a href="{{ route('compare.show', $compare_input_text->id) }}">
+                    {{ Str::limit($compare_input_text->content, 50) }} <!-- Display first 50 characters -->
                 </a>
             </li>
         @endforeach
